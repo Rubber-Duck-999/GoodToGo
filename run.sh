580b497d7010042461c1362b90fbdc8ac6f1309a
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-08-05 22:51:18 +0100

    Change the function name

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo 'Starting'
 python3 /home/pi/Documents/GoodToGo/main.py &
 
-python3 /home/pi/Documents/HouseGuardServices/Notifier/main.py
+python3 /home/pi/Documents/HouseGuardServices/Notifier/app.py
