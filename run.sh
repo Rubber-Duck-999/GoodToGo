2aa8363da3c21e0c59af51cae933a6138163f36c
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-08-05 22:47:38 +0100

    Update good to go

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 echo 'Starting'
-python3 /home/pi/Documents/GoodToGo/main.py
+python3 /home/pi/Documents/GoodToGo/main.py &
+
+python3 /home/pi/Documents/HouseGuardServices/Notifier/main.py
